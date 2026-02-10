1918ea038170ccb849aa175288185788cbb30d73
siva-sankaran <7930913+siva-sankaran@users.noreply.github.com>
2026-02-10 17:31:19 +0530

    done 3-3

--- List/main.py
+++ List/main.py
@@ -12,7 +12,15 @@ def print_each_item_of_list_with_greeting():
     print(f"Hello, {friends_names[2]}!")
     print(f"Hello, {friends_names[3]}!")
 
+def print_transport():
+    mode_of_transport = ['motorcycle', 'bicycle', 'boat', 'ship', 'train']
+    print(f"I want to own a {mode_of_transport[0]}")
+    print(f"I want to own a {mode_of_transport[1]}")
+    print(f"I want to own a {mode_of_transport[2]}")
+    print(f"I want to own a {mode_of_transport[3]}")
+    print(f"I want to own a {mode_of_transport[4]}")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_each_item_of_list_with_greeting()
+    print_transport()
 
