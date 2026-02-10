4bc10861f07285881491ec4c3b6d0a522adcfc88
siva-sankaran <7930913+siva-sankaran@users.noreply.github.com>
2026-02-10 14:40:13 +0530

    done 2 exercises of chapter 3 list

---
+++ List/main.py
@@ -0,0 +1,18 @@
+def print_each_item_of_list():
+    friends_names = ['Kumar', 'Ram', 'Suraj', 'Aundy' ]
+    print(friends_names[0])
+    print(friends_names[1])
+    print(friends_names[2])
+    print(friends_names[3])
+
+def print_each_item_of_list_with_greeting():
+    friends_names = ['Kumar', 'Ram', 'Suraj', 'Aundy']
+    print(f"Hello, {friends_names[0]}!")
+    print(f"Hello, {friends_names[1]}!")
+    print(f"Hello, {friends_names[2]}!")
+    print(f"Hello, {friends_names[3]}!")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_each_item_of_list_with_greeting()
+
